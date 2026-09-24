04a946d9e2c90a1c6e6ad546c2af32a6377a96de
MRDonnii <98255323+MRDonnii@users.noreply.github.com>
2026-09-24 23:18:46 +0200

    Prepare v0.3.94 release

--- dist/ha-smart-home-cards.js
+++ dist/ha-smart-home-cards.js
@@ -1,4 +1,4 @@
-/* MRDonnii Smart Home Cards v0.3.93 */
+/* MRDonnii Smart Home Cards v0.3.94 */
 
 // src/cards/ha-ai-usage-card/ha-card-list-editor.js
 var HACardListEditor = class extends HTMLElement {
